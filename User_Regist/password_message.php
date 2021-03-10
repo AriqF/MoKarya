2c21b58ebeeb183abf40b8a62616e05d0b0a7579
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-03-10 16:33:34 +0700

    add front end

--- User_Regist/password_message.php
+++ User_Regist/password_message.php
@@ -1,21 +1,66 @@
 <?php require_once 'controllers/authController.php'; ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Forgot Password</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
+    <head>
+        <title>MoKarya || Forgot Password</title>
+         <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+      
+        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        
+        <!--Swiper JS-->
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 
- <form>
-    <h1>C H E C K &nbsp; Y O U R <br>    E M A I L</h1>
+        <script src="https://unpkg.com/swiper/swiper-bundle.js"></script>
+        <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 
+        <!--My css-->
+        <link href="../src/css/registerStyle.css" type="text/css" rel="stylesheet"> 
 
-        <br>
-        <p style="text-align: center; color: white;">
-                An email has been sent to your email address to with a link to reset your
-                password.
-        </p>
- </form>
- 
+        <!--Swiper.js-->
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.css" />
+        <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+         
+        <!--Google Fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Epilogue&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <form form class="form" action="reset_password.php" method="post">
+        <div class="content">
+            <div class="container" id="box" style="padding: 0; margin-top: 20px;">
+                <div class="row">
+                    <div class="col-md-6">
+                        <!--Form Box-->
+                        <div class="container" id="formBox">
+                            <h2>Silahkan Periksa Email Anda</h2>
+
+                            <p style="font-size: 14px;">Email telah dikirimkan untuk mereset password anda</p>
+                            <hr style="border-top: 1px solid white; margin-bottom: 36px;">
+                            <div class="w-100"></div>
+                            <form>
+                                <!--add image here later-->
+                            </form>
+                            
+                        </div>
+                    </div>
+                    <!--Image Box-->
+                    <div class="col-md-6">
+                        <div class="container" id="pictBox">
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </form>
+    </body>
+</html>
