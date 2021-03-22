0ed58368c5802a3e1d753c00ef4509c8ef32fb8f
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-03-22 14:48:23 +0700

    Update unggahGallery-admin.php

--- User_Regist/unggahGallery-admin.php
+++ User_Regist/unggahGallery-admin.php
@@ -3,7 +3,7 @@
     include 'header-admin.php';
 ?>
 <section>
-<div class="container" id="box" style=" margin-top: 50px; color:white; padding-top: 30px;">
+<div class="container anim" id="box" style=" margin-top: 50px; color:white; padding-top: 30px; width:fit-content">
             <div class="d-flex justify-content-center">
                 <div >
                     <!--Form Box-->
