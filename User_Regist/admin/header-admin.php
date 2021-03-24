70e0cb6c94b0ac4fd6066ee05c581bc558a6aa44
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-03-24 21:34:02 +0700

    add coments

--- User_Regist/admin/header-admin.php
+++ User_Regist/admin/header-admin.php
@@ -44,7 +44,7 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #4527a4;">
-            <a class="navbar-brand" href="#">Welcome </a> 
+            <a class="navbar-brand" href="#">Welcome  <!--<.?php echo $_SESSION['user_admin']; ?> --></a> 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
