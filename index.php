bc9be2675d54e474be2002fe88f2d8b76fd1011c
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-03-30 21:09:25 +0700

    change img per slide to 3

--- index.php
+++ index.php
@@ -185,8 +185,9 @@
       <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
       <script>
         var swiper = new Swiper('.swiper-container', {
-          slidesPerView: 1,
+          slidesPerView: 3,
           spaceBetween: 30,
+          slidesPerGroup: 3,
           loop: true,
           loopFillGroupWithBlank: true,
           pagination: {
