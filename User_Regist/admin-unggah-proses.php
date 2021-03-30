32e264f308d71eace5d2512578202455661b3149
Ariq Fachry R <AriqF@users.noreply.github.com>
2021-03-30 10:57:15 +0700

    add restriction for image n empty form

--- User_Regist/admin-unggah-proses.php
+++ User_Regist/admin-unggah-proses.php
@@ -5,46 +5,56 @@
   $judul      = $_POST['judul'];
   $deskripsi  = $_POST['deskripsi'];
   $anggota    = $_POST['anggota'];
-  $foto_karya = $_FILES['foto_karya']['name'];
+  $foto_karya = $_FILES['foto_karya']['name']; 
 
-//cek dulu jika ada gambar produk jalankan coding ini
-if($foto_karya != "") {
-  $ekstensi_diperbolehkan = array('png','jpg'); //ekstensi file gambar yang bisa diupload 
-  $x = explode('.', $foto_karya); //memisahkan nama file dengan ekstensi yang diupload
-  $ekstensi = strtolower(end($x)); //manipulasi sebuah string menjadi huruf kecil.
-  $file_tmp = $_FILES['foto_karya']['tmp_name'];   
-  $angka_acak     = rand(1, 999);
-  $nama_gambar_baru = $angka_acak.'-'.$foto_karya; //menggabungkan angka acak dengan nama file sebenarnya
-        if(in_array($ekstensi, $ekstensi_diperbolehkan) === true)  {     
-                move_uploaded_file($file_tmp, 'gambar/'.$nama_gambar_baru); //memindah file gambar ke folder gambar
-                  // jalankan query INSERT untuk menambah data ke database pastikan sesuai urutan (id tidak perlu karena dibikin otomatis)
-                  $query = "INSERT INTO data_karya (judul, deskripsi, anggota,foto_karya) VALUES ('$judul', '$deskripsi', '$anggota', '$nama_gambar_baru')";
-                  $result = mysqli_query($conn, $query);
-                  // periska query apakah ada error
-                  if(!$result){
-                      die ("Query gagal dijalankan: ".mysqli_errno($conn).
-                           " - ".mysqli_error($conn));
-                  } else {
-                    //tampil alert dan akan redirect ke halaman index.php
-                    //silahkan ganti index.php sesuai halaman yang akan dituju
-                    echo "<script>alert('Data berhasil ditambah.');window.location='admin-gallery-data';</script>";
-                  }
 
-            } else {     
-             //jika file ekstensi tidak jpg dan png maka alert ini yang tampil
+  if (empty($judul) || empty($deskripsi) || empty($anggota)){
+    echo "<script>alert('Form wajib diisi dengan lengkap!');</script>"; 
+  }
+  //cek dulu jika ada gambar produk jalankan coding ini
+  if($foto_karya != "" ) {
+    $ekstensi_diperbolehkan = array('png','jpg'); //ekstensi file gambar yang bisa diupload 
+    $x = explode('.', $foto_karya); //memisahkan nama file dengan ekstensi yang diupload
+    $ekstensi = strtolower(end($x)); //manipulasi sebuah string menjadi huruf kecil.
+    $file_tmp = $_FILES['foto_karya']['tmp_name'];  
+    list($width, $height, $type, $attr) = getimagesize($file_tmp); 
+    $angka_acak     = rand(1, 999);
+    $nama_gambar_baru = $angka_acak.'-'.$foto_karya; //menggabungkan angka acak dengan nama file sebenarnya
+          if(in_array($ekstensi, $ekstensi_diperbolehkan) === true && $width === 1920 && $height === 1080)  {     
+                  move_uploaded_file($file_tmp, 'gambar/'.$nama_gambar_baru); //memindah file gambar ke folder gambar
+                    // jalankan query INSERT untuk menambah data ke database pastikan sesuai urutan (id tidak perlu karena dibikin otomatis)
+                    $query = "INSERT INTO data_karya (judul, deskripsi, anggota,foto_karya) VALUES ('$judul', '$deskripsi', '$anggota', '$nama_gambar_baru')";
+                    $result = mysqli_query($conn, $query);
+                    // periska query apakah ada error
+                    if(!$result){
+                        die ("Query gagal dijalankan: ".mysqli_errno($conn).
+                            " - ".mysqli_error($conn));
+                    } else {
+                      //tampil alert dan akan redirect ke halaman index.php
+                      //silahkan ganti index.php sesuai halaman yang akan dituju
+                      echo "<script>alert('Data berhasil ditambah.');window.location='admin-gallery-data';</script>";
+                    }
+
+              }
+              elseif($width != 1920 || $height != 1080){
+                echo "<script>alert('Gambar harus memiliki dimensi 1920x1080!');window.location='admin-unggah';</script>";
+              }
+
+              else {     
+                //jika file ekstensi tidak jpg dan png maka alert ini yang tampil
                 echo "<script>alert('Ekstensi gambar yang boleh hanya jpg atau png.');window.location='admin-unggah';</script>";
-            }
-} else {
-   $query = "INSERT INTO data_karya (judul, deskripsi, anggota, , foto_karya) VALUES ('$judul', '$deskripsi', '$anggota', '$', null)";
-                  $result = mysqli_query($conn, $query);
-                  // periska query apakah ada error
-                  if(!$result){
-                      die ("Query gagal dijalankan: ".mysqli_errno($conn).
-                           " - ".mysqli_error($conn));
-                  } else {
-                    //tampil alert dan akan redirect ke halaman index.php
-                    //silahkan ganti index.php sesuai halaman yang akan dituju
-                    echo "<script>alert('Data berhasil ditambah.');window.location='admin-gallery-data';</script>";
-                  }
-}
+              }
+  } else {
+    $query = "INSERT INTO data_karya (judul, deskripsi, anggota, , foto_karya) VALUES ('$judul', '$deskripsi', '$anggota', '$', null)";
+                    $result = mysqli_query($conn, $query);
+                    // periska query apakah ada error
+                    if(!$result){
+                        die ("Query gagal dijalankan: ".mysqli_errno($conn).
+                            " - ".mysqli_error($conn));
+                    } else {
+                      //tampil alert dan akan redirect ke halaman index.php
+                      //silahkan ganti index.php sesuai halaman yang akan dituju
+                      echo "<script>alert('Data berhasil ditambah.');window.location='admin-gallery-data';</script>";
+                    }
+  }
 ?>
--- User_Regist/admin-unggah.php
+++ User_Regist/admin-unggah.php
@@ -14,13 +14,13 @@
                         <div class="w-100"></div>
                         <form method="POST" action="admin-unggah-proses" enctype="multipart/form-data">
                             <label class="label control-label">Judul Karya</label>
-                            <input type="text" class="form-control" name="judul" placeholder="judul karya">
+                            <input type="text" class="form-control" name="judul" placeholder="judul karya" maxlength="15">
                             <label class="label control-label">Deskripsi</label>
-                            <input class="form-control" name="deskripsi" placeholder="tulis deskripsi karya secara singkat" style="margin-bottom: 10px;"></input>
+                            <input class="form-control" name="deskripsi" placeholder="tulis deskripsi karya secara singkat" style="margin-bottom: 10px;" maxlength="30" minlength="10"></input>
                             <label class="label control-label">Anggota</label>
                             <input type="text" class="form-control" name="anggota" placeholder="pisahkan dengan koma jika anggota > 1">
                             <label for="imageUpload">Unggah Foto Karya</label>
-                            <input name="foto_karya" type="file" class="form-control-file" id="exampleFormControlFile1" style="cursor: pointer;">                          
+                            <input name="foto_karya" type="file"  class="form-control-file" id="exampleFormControlFile1" style="cursor: pointer;">                          
                             <div class="w-100"></div>
                             <button name="submit-btn" type="submit" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" style="margin-top: 10px; width: 30%; border-radius: 5px" >
                                 Unggah
@@ -33,8 +33,17 @@
             </div>
         </div>
 
-
-
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            Swal.fire(
+            'Ketentuan Mengunggah Data',
+            '<ul> <li>Judul terdiri dari maksimal 15 karakter </li> <li>Deskripsi terdiri dari 10 hingga 30 karakter </li> <li>Foto yang diunggah memiliki ketentuan dengan ukuran 1920x1080</li></ul>',
+            'question'
+            )
+        });
+    </script>
 
     </body>
 </html>
