dd48384d0024a8ea46bf20ba007ac9aa7fe17143
muhammadalifh <61005674+muhammadalifh@users.noreply.github.com>
2021-03-31 14:59:14 +0700

    Update index.php
    add autoplay for gallery

--- index.php
+++ index.php
@@ -205,6 +205,10 @@
             el: '.swiper-pagination',
             clickable: true,
           },
+          autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+           },
           navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
